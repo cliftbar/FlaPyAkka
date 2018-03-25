3820ad4836a8a293e65da13714d2468505213331
cliftbar <cwbarclift@gmail.com>
2018-03-25 17:33:39 -0400

    first commit

---
+++ src/main/scala/com/cliftbar/flapyakka/FlaPyAkkaController.scala
@@ -0,0 +1,184 @@
+package com.cliftbar.flapyakka
+
+import com.typesafe.config.ConfigFactory
+import akka.http.scaladsl.server.{HttpApp, Route}
+import spray.json.DefaultJsonProtocol._
+import spray.json._
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+import akka.http.scaladsl.model.headers.RawHeader
+import cliftbar.disastermodeling.hurricane.TrackPoint
+import cliftbar.disastermodeling.hurricane.{nws23 => nws}
+
+// Server model definition
+//  Defines routes and converts incoming http data to scala types before
+//  passing data AkkaDisasterApp.  Also converts scala types back to appropriate form for http.
+object FlaPyAkkaController extends HttpApp with App {
+  // Instance of model class
+  val model = new FlaPyAkkaModel
+  val serverId = (math.random() * 1000).toInt
+  // Routes that this WebServer must handle are defined here
+  def routes: Route =
+  pathEndOrSingleSlash { // Listens to the top "/"
+      get {
+        complete("home response")
+      }
+//    respondWithHeaders(RawHeader("id", server_id.toString), RawHeader("another_header", "hi")) {
+//      complete("Server " + server_id.toString + " up and running") // Completes with some text
+//    }
+  } ~
+    path("health") { // health check the server, gives some info and such
+      get { // Listens only to GET requests
+        respondWithHeaders(RawHeader("server_id", this.serverId.toString), RawHeader("QueryTime", LocalDateTime.now().toString)) {
+          complete("healthCheck")
+        }
+      }
+    } ~
+//    path("returnJson") { // Path to return json
+//      get {
+//        val j = Map("imageUri" -> "path\\here", "nws" -> nws.model.Rho0_kPa.toString)
+//        complete(j.toJson.toString) // Completes with some text
+//      }
+//    } ~
+    path("peset") { // Path to return json
+      post {
+        entity(as[String]) { json =>
+          val parsedJson = JsonParser(json).asJsObject
+          val resetType:String = parsedJson.fields("resetType").convertTo[String]
+          respondWithHeader(RawHeader("server id", this.serverId.toString))
+          complete("success")
+        }
+      }
+    } ~
+    path("calculate" / "hurricane" / "nws23") { // Calculate a hurricane.  Parameters and track passed in with JSON
+      withoutRequestTimeout {
+        get {
+          entity(as[String]) { json =>
+            val parsedJson = JsonParser(json).asJsObject
+            //println(json)
+
+            // Get top level fields
+            val maxDist: Int = parsedJson.fields("maxDist").convertTo[Int]
+            val par: Int = parsedJson.fields("par").convertTo[Int]
+            val fspeed: Option[Double] = parsedJson.fields.get("fspeed").map(x => x.convertTo[Double])
+            val rmax: Double = parsedJson.fields("rmax").convertTo[Double]
+            val callId: String = parsedJson.fields("call_id").convertTo[String]
+
+            //get Bbox without custom protocol
+            val jsonBbox = parsedJson.fields("BBox").asJsObject
+            val pxPerDegreeX: Int = jsonBbox.fields("pxPerDegreeX").convertTo[Int]
+            val pxPerDegreeY: Int = jsonBbox.fields("pxPerDegreeY").convertTo[Int]
+            val botLatY: Float = jsonBbox.fields("botLatY").convertTo[Float]
+            val topLatY: Float = jsonBbox.fields("topLatY").convertTo[Float]
+            val rightLonX: Float = jsonBbox.fields("rightLonX").convertTo[Float]
+            val leftLonX: Float = jsonBbox.fields("leftLonX").convertTo[Float]
+            val bBox = new nws.BoundingBox(topLatY, botLatY, leftLonX, rightLonX)
+
+            // Custom Protocol for Parsing track points in the json.
+            //  This is "our" job, not the libraries, the library only defines the case class
+            //  since it doesn't know how the data will be parsed.
+            object TrackPointJsonProtocol extends DefaultJsonProtocol with NullOptions {
+
+              implicit object TrackPointJsonFormat extends RootJsonFormat[TrackPoint] {
+                val dtFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm")
+
+                def write(tp: TrackPoint) = JsObject(
+                  "catalogNumber" -> tp.catalogNumber.map(JsNumber(_)).getOrElse(JsNull)
+                  , "stormName" -> JsString(tp.stormName)
+                  , "basin" -> tp.basin.map(JsString(_)).getOrElse(JsNull)
+                  , "timestamp" -> JsString(tp.timestamp.format(dtFormatter))
+                  , "eyeLat_y" -> JsNumber(tp.eyeLat_y)
+                  , "eyeLon_x" -> JsNumber(tp.eyeLon_x)
+                  , "maxWind_kts" -> tp.maxWind_kts.map(JsNumber(_)).getOrElse(JsNull)
+                  , "minCp_mb" -> tp.minCp_mb.map(JsNumber(_)).getOrElse(JsNull)
+                  , "sequence" -> JsNumber(tp.sequence)
+                  , "fSpeed_kts" -> JsNumber(tp.fSpeed_kts)
+                  , "isLandfallPoint" -> JsBoolean(tp.isLandfallPoint)
+                  , "rMax_nmi" -> JsNumber(tp.rMax_nmi)
+                  , "gwaf" -> JsNumber(tp.gwaf)
+                  , "heading" -> tp.heading.map(JsNumber(_)).getOrElse(JsNull)
+                )
+
+                def read(value: JsValue) = value match {
+                  case JsObject(fields) => {
+                    val catalogNumber: Option[Int] = fields("catalogNumber").convertTo[Option[Int]]
+                    val stormName: String = fields("stormName").convertTo[String]
+                    val basin: Option[String] = fields("basin").convertTo[Option[String]]
+                    val timestamp: String = fields("timestamp").convertTo[String]
+                    val eyeLat_y: Double = fields("eyeLat_y").convertTo[Double]
+                    val eyeLon_x: Double = fields("eyeLon_x").convertTo[Double]
+                    val maxWind_kts: Option[Double] = fields("maxWind_kts").convertTo[Option[Double]]
+                    val minCp_mb: Option[Double] = fields("minCp_mb").convertTo[Option[Double]]
+                    val sequence: Double = fields("sequence").convertTo[Double]
+                    val fSpeed_kts: Double = fields("fSpeed_kts").convertTo[Double]
+                    val isLandfallPoint: Boolean = fields("isLandfallPoint").convertTo[Boolean]
+                    val rMax_nmi: Double = fields("rMax_nmi").convertTo[Double]
+                    val gwaf: Double = fields("gwaf").convertTo[Double]
+                    val heading: Option[Double] = fields("heading").convertTo[Option[Double]]
+
+                    new TrackPoint(
+                      catalogNumber
+                      , stormName
+                      , basin
+                      , LocalDateTime.parse(timestamp, dtFormatter)
+                      , eyeLat_y
+                      , eyeLon_x
+                      , maxWind_kts
+                      , minCp_mb
+                      , sequence
+                      , fSpeed_kts
+                      , isLandfallPoint
+                      , rMax_nmi
+                      , gwaf
+                      , heading
+                    )
+                  }
+                  case _ => deserializationError("Track Point expected")
+                }
+              }
+
+            }
+
+            import TrackPointJsonProtocol._
+            val track = parsedJson.fields("track").convertTo[Seq[TrackPoint]]
+
+            //println("parsed successfully")
+            //println(track.toJson)
+
+            // Run calculation
+            val retMap = model.CalculateHurricane(track, bBox, fspeed, rmax, (pxPerDegreeX, pxPerDegreeY), maxDist, par)
+            //println(retMap)
+            // package return map as json to send back to caller
+            val retJson = retMap.toJson
+            //complete(retJson.toString) // Return the image name as JSON string
+            import java.io.File
+            import akka.http.scaladsl.model.{HttpEntity, MediaTypes}
+
+            import java.nio.file.Files
+            val fi = new File(retMap("imageUri"))
+            val fileContent = Files.readAllBytes(fi.toPath)
+
+            val responseEntity = HttpEntity(
+              MediaTypes.`image/png`
+              ,fileContent
+            )
+            println("ServerID " + serverId.toString + ", call_id: " + callId.toString)
+            respondWithHeaders(RawHeader("call_id", callId.toString), RawHeader("server_id", serverId.toString)) {
+              complete(responseEntity)
+            }
+          }
+        }
+      }
+    }
+
+
+  // This will start the server until the return key is pressed
+  val httpConfig = ConfigFactory.load().getConfig("akka.http")
+  val interface = httpConfig.getString("server.interface")
+  val port = httpConfig.getInt("server.port")
+  //println(interface)
+  //println(port)
+  println("ServerID " + serverId.toString + " at " + interface.toString + ":" + port.toString)
+  startServer(interface, port)
+}
---
+++ src/main/scala/com/cliftbar/flapyakka/FlaPyAkkaModel.scala
@@ -0,0 +1,32 @@
+package com.cliftbar.flapyakka
+
+import java.io.FileWriter
+import java.nio.file.Paths
+import java.time
+
+import cliftbar.disastermodeling.hurricane.TrackPoint
+import cliftbar.disastermodeling.hurricane.nws23._
+
+
+class FlaPyAkkaModel {
+  def home
+  def CalculateHurricane(trackPoints:Seq[TrackPoint], bBox:BoundingBox, fSpeed_kts:Option[Double], rMax_nmi:Double, pxPerDegree:(Int, Int), maxDist:Int, par:Int = -1):Map[String, String] = {
+
+    //println("Start Calculate Hurricane with NWS23")
+    //println(time.LocalDateTime.now())
+    val grid = new LatLonGrid(bBox.topLatY, bBox.botLatY, bBox.leftLonX, bBox.rightLonX, pxPerDegree._1, pxPerDegree._2)
+    val event = new HurricaneEvent(grid, trackPoints.toList, rMax_nmi)
+
+    val imageName = "test_image.png"
+    event.DoCalcs(maxDist, par, imageName, "test_text.txt")
+
+    //println(time.LocalDateTime.now())
+    //println("Did test")
+
+    val retPath = Paths.get(System.getProperty("user.dir"), imageName)
+    //val retPath = System.getProperty("user.dir") + "\\" + imageName
+    val ret = Map("imageUri" -> retPath.toAbsolutePath.toString)
+
+    return ret
+  }
+}
